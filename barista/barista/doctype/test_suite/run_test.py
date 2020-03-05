9188827bedcb231571e6b3ef02ae3a408b1cb12c
Maithilee <maithilee.inge@elastic.run>
2020-03-05 16:54:10 +0530

    Execution of named suites functionality added

--- barista/barista/doctype/test_suite/run_test.py
+++ barista/barista/doctype/test_suite/run_test.py
@@ -14,25 +14,37 @@ from barista.barista.doctype.test_case.test_case_execution import  TestCaseExecu
 class RunTest():
 
     #Run all the suites for the given app
-    def run_complete_suite(self, app_name):
+    def run_complete_suite(self, app_name,suites=[]):
 
         print("\033[0;33;93m************ Running all test cases for App - " + app_name + "*************\n\n")
-
-        suites = frappe.get_all("Test Suite", filters={'app_name' : app_name},order_by='creation asc')
+        if len(suites)==0:
+            suites = frappe.get_all("Test Suite", filters={'app_name' : app_name},order_by='creation asc')
+        else:
+            suite_name=[]
+            for suite in suites:
+                suite_name.append({'name':suite})
+            suites=suite_name
+            
+    
+        import shutil
+        path=frappe.get_app_path('barista') + '/public/test-coverage/'
+        shutil.rmtree(path,ignore_errors=True)
         generatorObj = TestDataGenerator()        
         objCoverage = coverage.Coverage(source=[frappe.get_app_path(app_name)] )
         objCoverage.start()
         
         for suite in suites:
+
+            print("\033[0;32;92m************ Suite - " + suite.get('name') + "*************\n\n")
             try:
-                generatorObj.create_pretest_data(suite['name'])            
-                testcases = frappe.get_list('Testcase Item' , filters={'parent': suite['name']}, fields=["testcase"],order_by="idx")
+                generatorObj.create_pretest_data(suite.get('name'))            
+                testcases = frappe.get_list('Testcase Item' , filters={'parent': suite.get('name')}, fields=["testcase"],order_by="idx")
                 # print("\n \ntestcases---",testcases)  
                 for testcase in testcases:                    
                     self.run_testcase(testcase,suite)
 
             except Exception as e:
-                print("\033[0;31;91mAn Error occurred which will cause false test case result in the suite - " + str(suite.name) )
+                print("\033[0;31;91mAn Error occurred which will cause false test case result in the suite - " + str(suite.get('name')) )
                 print("\033[0;31;91m*************ERROR****************")
                 print("\033[0;31;91m The error encountered is - " + str(e)  + "\n")
                 print("\033[0;31;91m*************ERROR****************")
@@ -50,5 +62,5 @@ class RunTest():
     
     def run_testcase(self, testcase, suite):
         executionObj = TestCaseExecution()        
-        executionObj.run_testcase(testcase['testcase'], suite['name'])
+        executionObj.run_testcase(testcase['testcase'], suite.get('name'))
         frappe.db.commit()
