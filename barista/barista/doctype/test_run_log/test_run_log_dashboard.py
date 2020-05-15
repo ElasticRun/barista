d72b072cf65b78d607896ed684943f76c2b83657
Abhishek Kedar <abhishek.kedar@elastic.run>
2020-05-15 17:53:03 +0530

    committed empty dashboard test run log

---
+++ barista/barista/doctype/test_run_log/test_run_log_dashboard.py
@@ -0,0 +1,18 @@
+from frappe import _
+
+
+def get_data():
+    return {
+        'fieldname': 'test_run_name',
+        'non_standard_fieldnames': {
+            'Test Result': 'test_run_name'
+        },
+        'internal_links': {
+
+        },
+        'transactions': [
+            {
+                'items': []
+            },
+        ]
+    }
