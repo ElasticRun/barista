feb76b37a4a4b718ee4470a7fa2bd47c1aa65f26
abhishek.kedar <abhishek.kedar@elastic.run>
2020-07-07 10:50:27 +0530

    user parameter added in bench run-barista command

--- barista/commands.py
+++ barista/commands.py
@@ -9,16 +9,17 @@ from barista.barista.doctype.test_suite.run_test import RunTest, resolve_run_nam
 @click.argument('app_name')
 @click.option('-r', '--run-name', default='Pass-1', help='Test Run Name for this execution run')
 @click.option('-s', '--suite', multiple=True, help='Test Suite name')
+@click.option('-u', '--user', default='Administrator', help='Set user for execution')
 @pass_context
-def run_barista(context, app_name, suite=[], run_name='Pass-1'):
+def run_barista(context, app_name, suite=[], run_name='Pass-1',user='Administrator'):
     site = get_site(context)
     frappe.init(site=site)
     frappe.connect(site)
-
+    frappe.set_user(user)
     run_name = resolve_run_name(run_name)
     print('Test Run Name - ', run_name)
     RunTest().run_complete_suite(app_name, list(suite), run_name)
-
+   
 @click.command('clear-error-log')
 @pass_context
 def clear_error_log(context):
