5012c776db9f0eb779f7608cf084ec03a2c632ef
Sudhanshu Kulkarni <sudhanshu.kulkarni@elastic.run>
2022-06-09 17:27:17 +0530

    Info icon on test cases

--- barista/barista/doctype/test_case/test_case.py
+++ barista/barista/doctype/test_case/test_case.py
@@ -33,3 +33,21 @@ class TestCase(Document):
 	def on_update(self):
 		if self.is_standard == "Yes" and frappe.local.conf.developer_mode and not frappe.flags.in_migrate:
 			export_to_files(record_list=[[self.doctype, self.name]], record_module="barista", create_init=True)
+
+
+# Get data for test-case-info:
+@frappe.whitelist()
+def get_test_case_info(test_case):
+	# Get relevant data to pass to the template:
+	test_case_doc = frappe.get_doc("Test Case", test_case).as_dict()
+	assertions = test_case_doc.get("assertion", {})
+	test_data_name, test_data_description = frappe.get_value("Test Data", test_case_doc.test_data, ['name', 'description'])
+
+	# render template and fill JINJA values:
+	rendered_info = frappe.render_template('barista/barista/doctype/test_case/templates/test_case_info.html',
+											{'test_case_doc': test_case_doc,
+											'test_data_name': test_data_name,
+											'test_data_description': test_data_description,
+											'assertions': assertions})
+	
+	return rendered_info
